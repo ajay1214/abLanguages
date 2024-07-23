37ffd00abdc53c074769401da0dc1aeb8f0a8e40
Ajay Bind <adarshbind61@gmail.com>
2024-07-23 15:57:48 +0530

    Course updated

--- constants/index.ts
+++ constants/index.ts
@@ -1,78 +1,9 @@
-export const footerFlags = [
-  {
-    src: "/hindi.svg",
-    label: "Hindi",
-  },
-  {
-    src: "/hr.svg",
-    label: "Croatian",
-  },
-  {
-    src: "/es.svg",
-    label: "Spanish",
-  },
-  {
-    src: "/fr.svg",
-    label: "French",
-  },
-  {
-    src: "/it.svg",
-    label: "Italian",
-  },
-  {
-    src: "/jp.svg",
-    label: "Japanese",
-  },
-];
-
-export const sidebarItems = [
-  {
-    link: "/learn",
-    label: "Learn",
-    iconSrc: "/learn.svg",
-  },
-  {
-    link: "/leaderboard",
-    label: "Leaderboard",
-    iconSrc: "/leaderboard.svg",
-  },
-  {
-    link: "/quests",
-    label: "Quests",
-    iconSrc: "/quests.svg",
-  },
-  {
-    link: "/shop",
-    label: "Shop",
-    iconSrc: "/shop.svg",
-  },
-];
-
-export const quests = [
-  {
-    title: "Earn 20 XP",
-    value: 20,
-  },
-  {
-    title: "Earn 50 XP",
-    value: 50,
-  },
-  {
-    title: "Earn 100 XP",
-    value: 100,
-  },
-  {
-    title: "Earn 500 XP",
-    value: 500,
-  },
-  {
-    title: "Earn 1000 XP",
-    value: 1000,
-  },
-];
-
 export const dataToInsert = {
   languages: [
+    {
+      title: "Hindi",
+      imgSrc: "/hindi.svg",
+    },
     {
       title: "Spanish",
       imgSrc: "/es.svg",
@@ -97,9 +28,16 @@ export const dataToInsert = {
   units: [
     {
       id: 1,
-      courseId: 1, // Spanish
+      courseId: 2, // Spanish
       title: "Unit 1",
       description: "Learn the basics of Spanish",
+      order: 1,
+    },
+    {
+      id: 2,
+      courseId: 1, // Hindi
+      title: "Unit 1",
+      description: "Learn the basics of Hindi",
       order: 1,
     },
   ],
@@ -113,6 +51,18 @@ export const dataToInsert = {
     {
       id: 2,
       unitId: 1, // Unit 1 (Learn the basics...)
+      order: 2,
+      title: "Verbs",
+    },
+    {
+      id: 3,
+      unitId: 2, // Unit 1 (Learn the basics of Hindi)
+      order: 1,
+      title: "Nouns",
+    },
+    {
+      id: 4,
+      unitId: 2, // Unit 1 (Learn the basics of Hindi)
       order: 2,
       title: "Verbs",
     },
@@ -156,6 +106,48 @@ export const dataToInsert = {
     {
       id: 6,
       lessonId: 2, // Verbs
+      type: "SELECT",
+      order: 3,
+      question: 'Which one of these is the "the woman"?',
+    },
+    {
+      id: 7,
+      lessonId: 3, // Nouns (Hindi)
+      type: "SELECT",
+      order: 1,
+      question: 'Which one of these is the "the man"?',
+    },
+    {
+      id: 8,
+      lessonId: 3, // Nouns (Hindi)
+      type: "ASSIST",
+      order: 2,
+      question: '"the man"',
+    },
+    {
+      id: 9,
+      lessonId: 3, // Nouns (Hindi)
+      type: "SELECT",
+      order: 3,
+      question: 'Which one of these is the "the robot"?',
+    },
+    {
+      id: 10,
+      lessonId: 4, // Verbs (Hindi)
+      type: "SELECT",
+      order: 1,
+      question: 'Which one of these is the "the man"?',
+    },
+    {
+      id: 11,
+      lessonId: 4, // Verbs (Hindi)
+      type: "ASSIST",
+      order: 2,
+      question: '"the robot"',
+    },
+    {
+      id: 12,
+      lessonId: 4, // Verbs (Hindi)
       type: "SELECT",
       order: 3,
       question: 'Which one of these is the "the woman"?',
@@ -281,6 +273,126 @@ export const dataToInsert = {
       correct: false,
       text: "el robot",
       audioSrc: "/es_robot.mp3",
+    },
+    {
+      challengeId: 7, // Which one of these is "the man" (Hindi)?
+      imgSrc: "/man.svg",
+      correct: true,
+      text: "आदमी",
+      audioSrc: "/hindi_man.mp3",
+    },
+    {
+      challengeId: 7,
+      imgSrc: "/woman.svg",
+      correct: false,
+      text: "महिला",
+      audioSrc: "/hindi_woman.mp3",
+    },
+    {
+      challengeId: 7,
+      imgSrc: "/robot.svg",
+      correct: false,
+      text: "रोबोट",
+      audioSrc: "/hindi_robot.mp3",
+    },
+    {
+      challengeId: 8, // "the man" (Hindi)?
+      correct: true,
+      text: "आदमी",
+      audioSrc: "/hindi_man.mp3",
+    },
+    {
+      challengeId: 8,
+      correct: false,
+      text: "महिला",
+      audioSrc: "/hindi_woman.mp3",
+    },
+    {
+      challengeId: 8,
+      correct: false,
+      text: "रोबोट",
+      audioSrc: "/hindi_robot.mp3",
+    },
+    {
+      challengeId: 9, // Which one of these is the "the robot" (Hindi)?
+      imgSrc: "/man.svg",
+      correct: false,
+      text: "आदमी",
+      audioSrc: "/hindi_man.mp3",
+    },
+    {
+      challengeId: 9,
+      imgSrc: "/woman.svg",
+      correct: false,
+      text: "महिला",
+      audioSrc: "/hindi_woman.mp3",
+    },
+    {
+      challengeId: 9,
+      imgSrc: "/robot.svg",
+      correct: true,
+      text: "रोबोट",
+      audioSrc: "/hindi_robot.mp3",
+    },
+    {
+      challengeId: 10, // Which one of these is "the man" (Hindi)?
+      imgSrc: "/man.svg",
+      correct: true,
+      text: "आदमी",
+      audioSrc: "/hindi_man.mp3",
+    },
+    {
+      challengeId: 10,
+      imgSrc: "/woman.svg",
+      correct: false,
+      text: "महिला",
+      audioSrc: "/hindi_woman.mp3",
+    },
+    {
+      challengeId: 10,
+      imgSrc: "/robot.svg",
+      correct: false,
+      text: "रोबोट",
+      audioSrc: "/hindi_robot.mp3",
+    },
+    {
+      challengeId: 11, // "the robot" (Hindi)?
+      correct: false,
+      text: "आदमी",
+      audioSrc: "/hindi_man.mp3",
+    },
+    {
+      challengeId: 11,
+      correct: false,
+      text: "महिला",
+      audioSrc: "/hindi_woman.mp3",
+    },
+    {
+      challengeId: 11,
+      correct: true,
+      text: "रोबोट",
+      audioSrc: "/hindi_robot.mp3",
+    },
+    {
+      challengeId: 12, // Which one of these is the "the woman" (Hindi)?
+      imgSrc: "/man.svg",
+      correct: false,
+      text: "आदमी",
+      audioSrc: "/hindi_man.mp3",
+    },
+    {
+      challengeId: 12,
+      imgSrc: "/woman.svg",
+      correct: true,
+      text: "महिला",
+      audioSrc: "/hindi_woman.mp3",
+    },
+    {
+      challengeId: 12,
+      imgSrc: "/robot.svg",
+      correct: false,
+      text: "रोबोट",
+      audioSrc: "/hindi_robot.mp3",
     },
   ],
 };
