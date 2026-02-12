bed1c0867142f6701a2e17035399444e16661446
Ajay Bind <adarshbind61@gmail.com>
2026-02-12 09:29:35 +0530

    fix clerk matcher

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,17 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/", "/api/webhooks/stripe"]);
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/api/webhooks/stripe"
+]);
 
 export default clerkMiddleware((auth, req) => {
-  if (isPublicRoute(req)) return; // if it's a public route, do nothing
-  auth().protect(); // for any other route, require auth
+  if (isPublicRoute(req)) return;
+  auth().protect();
 });
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    "/((?!_next|.*\\..*).*)",
+  ],
 };
